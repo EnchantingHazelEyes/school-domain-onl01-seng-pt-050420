41377747d179765cb2d193ce18744e5a5064e477
Samantha Barros <samantha.barros95@gmail.com>
2020-06-08 19:53:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,8 +12,8 @@ class School
 
     def add_student(name, grade)
      # if roster[grade] != nil
-      roster[grade] ||= []
-      roster << name
+      roster ||= []
+      roster[grade] << name
      # end
     end
 end
