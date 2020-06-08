b4f1712d4906acda0f9847029c26534291b16ba2
Samantha Barros <samantha.barros95@gmail.com>
2020-06-08 19:12:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class School
+   # school = School.new() #("Bayside High School")
+    #school.roster
+    
+    attr_accessor :name, :roster
+    
+
+    def initialize(name)
+        @name = name
+        @roster = []
+    end
+
+    def add_student(name, grade)
+      @roster << name
+    end
+
+end
